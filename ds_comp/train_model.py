a03e3b746da49b0cdac6cc28b9abe2c9e4d387b8
a-avishka <a.avishka99@gmail.com>
2020-05-26 23:08:38 +0530

    Fixed a frontend bug

--- ds_comp/train_model.py
+++ ds_comp/train_model.py
@@ -74,18 +74,6 @@ val_loss = history_dict['val_loss']
 
 epochs = range(1, len(acc) + 1)
 
-# # "bo" is for "blue dot"
-# plt.plot(epochs, loss, 'bo', label='Training loss')
-# # b is for "solid blue line"
-# plt.plot(epochs, val_loss, 'b', label='Validation loss')
-# plt.title('Training and validation loss')
-# plt.xlabel('Epochs')
-# plt.ylabel('Loss')
-# plt.legend()
-#
-# plt.show()
-
-
 plt.clf()   # clear figure
 
 plt.plot(epochs, acc, 'bo', label='Training acc')
